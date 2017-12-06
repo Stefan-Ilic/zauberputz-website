0ae3f798c4fddec6b6301c59a6932c9969951c07
Stefan Ilic <stefan.ilic.vie@gmail.com>
2017-12-06 12:30:26 +0100

    Add content and basic formatting to index.php

--- index.php
+++ index.php
@@ -10,6 +10,41 @@
 
     <div class="container">
         <?php include("navbar.php"); ?>
+
+        <div class="row">
+            <section class="col-xs-12 col-md-4">
+                <h2>Hausbetreuung</h2>
+                <img src="img/sub_pic_1.png" alt="Hausbetreuung">
+                <p>Mit unserer Hausbetreuung versichern wir, dass Ihr Objekt über das ganze Jahr hinweg jederzeit sauber, gepflegt und betreut ist.</p>
+                <button class="btn btn-default">Mehr</button>
+            </section>
+            <section class="col-xs-12 col-md-4">
+                <h2>Grünflächenbetreuung</h2>
+                <img src="img/sub_pic_2.png" alt="Grünflächenbetreuung">
+                <p>Die Pflege, Sicherheit und Betreuung der Grünflächen Ihres Objektes ist uns ein großes Anliegen. Ob es um Ihren Rasen, Ihre Hecke oder Ihre Bäume geht, wir kümmern uns darum.</p>
+                <button class="btn btn-default">Mehr</button>
+            </section>
+            <section class="col-xs-12 col-md-4">
+                <h2>Winterdienst</h2>
+                <img src="img/sub_pic_3.png" alt="Winterdienst">
+                <p>In den kalten Monaten stellen wir sicher, dass Ihre Gehwege und sonstigen Flächen frei von Schnee und Glatteis sind, um größte Sicherheit zu gewährleisten.</p>
+                <button class="btn btn-default">Mehr</button>
+            </section>
+        </div>
+        <div class="row">
+            <section class="col-xs-12 col-md-4">
+                <h2>Über Uns</h2>
+                <img src="img/beschreibung_pic.png" alt="Über Uns">
+            </section>
+            <section class="col-xs-12 col-md-4">
+                <p>Ob es um die Sauberkeit Ihres Wohnobjektes, die Betreuung Ihrer Grünflächen oder die Räumung von Schneefall geht, wir sind das ganze Jahr für Sie und Ihr Objekt da und gehen gerne auf Ihre persönlichen Anliegen und Wünsche ein.</p>
+                <button class="btn btn-default">Mehr</button>
+            </section>
+            <section class="col-xs-12 col-md-4">
+                <p>Interessiert an einem Job bei uns?</p>
+                <button class="btn btn-default">Mehr</button>
+            </section>
+        </div>
     </div>
 
 </body>
