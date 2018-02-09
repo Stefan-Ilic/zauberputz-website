a8755e6dc2e048b95efa78ea62c567d4715451b7
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-02-09 11:38:18 +0100

    Change navbar to work better with Bootstrap's collapse

--- navbar.php
+++ navbar.php
@@ -1,28 +1,30 @@
-<nav class="navbar">
-    <div>
-        <div class="navbar-brand" href="index.php">
-            <span class="emmett">Zauberputz</span>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><span class="emmett">Zauberputz</span></a>
         </div>
-        <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="fa fa-bars"> </span>
-        </button>
-    </div>
-    <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav pull-right">
-            <li><a href="#"> Startseite </a></li>
-            <li class="dropdown">
-                <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                    Leistungen <span class="caret"></span>
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a href="#"> Hausbetreuung </a></li>
-                    <li><a href="#"> Grünflächenbetreuung </a></li>
-                    <li><a href="#"> Winterdienst </a></li>
-                </ul>
-            </li>
-            <li><a href="#"> Über Uns </a></li>
-            <li><a href="#"> Jobs </a></li>
-            <li><a href="#"> Kontakt </a></li>
-        </ul>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a class="navbar-item" href="index.php"> Startseite </a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle navbar-item" data-toggle="dropdown" href="#">
+                        Leistungen <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="navbar-item" href="#"> Hausbetreuung </a></li>
+                        <li><a class="navbar-item" href="#"> Grünflächenbetreuung </a></li>
+                        <li><a class="navbar-item" href="#"> Winterdienst </a></li>
+                    </ul>
+                </li>
+                <li><a class="navbar-item" href="#"> Über Uns </a></li>
+                <li><a class="navbar-item" href="#"> Jobs </a></li>
+                <li><a class="navbar-item" href="#"> Kontakt </a></li>
+            </ul>
+        </div>
     </div>
 </nav>
