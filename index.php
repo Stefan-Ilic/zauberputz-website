80dcc375818383cd5b085dabd65c9cef3e74db77
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-02-09 12:30:00 +0100

    Make carousel responsive

--- index.php
+++ index.php
@@ -7,20 +7,24 @@
 </head>
 
 <body>
-    <div class="container">
-        <?php include("navbar.php"); ?>
-        <?php include("carousel.php"); ?>
+<div class="container">
+    <?php include("navbar.php"); ?>
+    <?php include("carousel.php"); ?>
 
-        <div class="container">
+    <div class="container">
         <div class="row">
             <section class=" col-xs-12 col-md-4">
                 <h2>Hausbetreuung</h2>
                 <div class="block-center">
-                <figure>
-                    <img src="img/sub_pic_1.png" alt="Hausbetreuung">
-                    <figcaption class="text-limit">Mit unserer Hausbetreuung versichern wir, dass Ihr Objekt über das ganze Jahr hinweg jederzeit sauber, gepflegt und betreut ist.</figcaption>
-                    <figcaption><button class="btn btn-default pull-right zauberputz-button">Mehr</button></figcaption>
-                </figure>
+                    <figure>
+                        <img src="img/sub_pic_1.png" alt="Hausbetreuung">
+                        <figcaption class="text-limit">Mit unserer Hausbetreuung versichern wir, dass Ihr Objekt über
+                            das ganze Jahr hinweg jederzeit sauber, gepflegt und betreut ist.
+                        </figcaption>
+                        <figcaption>
+                            <button class="btn btn-default pull-right zauberputz-button">Mehr</button>
+                        </figcaption>
+                    </figure>
                 </div>
             </section>
 
@@ -28,42 +32,57 @@
                 <h2>Grünflächenbetreuung</h2>
                 <figure>
                     <img src="img/sub_pic_2.png" alt="Grünflächenbetreuung">
-                    <figcaption class="text-limit">Die Pflege, Sicherheit und Betreuung der Grünflächen Ihres Objektes ist uns ein großes Anliegen. Ob es um Ihren Rasen, Ihre Hecke oder Ihre Bäume geht, wir kümmern uns darum.</figcaption>
-                    <figcaption><button class="btn btn-default pull-right zauberputz-button">Mehr</button></figcaption>
+                    <figcaption class="text-limit">Die Pflege, Sicherheit und Betreuung der Grünflächen Ihres Objektes
+                        ist uns ein großes Anliegen. Ob es um Ihren Rasen, Ihre Hecke oder Ihre Bäume geht, wir kümmern
+                        uns darum.
+                    </figcaption>
+                    <figcaption>
+                        <button class="btn btn-default pull-right zauberputz-button">Mehr</button>
+                    </figcaption>
                 </figure>
             </section>
             <section class="col-xs-12 col-md-4 index-box">
                 <h2>Winterdienst</h2>
                 <figure>
                     <img src="img/sub_pic_3.png" alt="Winterdienst">
-                    <figcaption class="text-limit">In den kalten Monaten stellen wir sicher, dass Ihre Gehwege und sonstigen Flächen frei von Schnee und Glatteis sind, um größte Sicherheit zu gewährleisten.</figcaption>
-                    <figcaption><button class="btn btn-default pull-right zauberputz-button">Mehr</button></figcaption>
-                </figure>
-            </section>
-        </div>
-        </div>
-
-        <div class="row">
-            <section class="col-xs-12 col-md-4">
-                <h2>Über Uns</h2>
-                <figure>
-                    <img src="img/beschreibung_pic.png" alt="Ueber Uns">
-                    <figcaption class="text-limit">Wir sind das ganze Jahr für Sie und Ihr Objekt da und gehen gerne auf Ihre persönlichen Anliegen und Wünsche ein.</figcaption>
-                    <figcaption><button class="btn btn-default pull-right zauberputz-button">Mehr</button></figcaption>
-                </figure>
-            </section>
-            <section class="col-xs-12 col-md-4">
-                <h2>Jobs</h2>
-                <figure>
-                    <img src="img/jobs.jpg" alt="Jobs">
-                    <figcaption class="text-limit">Interessiert an einem Job bei uns?</figcaption>
-                    <figcaption><button class="btn btn-default pull-right zauberputz-button">Mehr</button></figcaption>
+                    <figcaption class="text-limit">In den kalten Monaten stellen wir sicher, dass Ihre Gehwege und
+                        sonstigen Flächen frei von Schnee und Glatteis sind, um größte Sicherheit zu gewährleisten.
+                    </figcaption>
+                    <figcaption>
+                        <button class="btn btn-default pull-right zauberputz-button">Mehr</button>
+                    </figcaption>
                 </figure>
             </section>
         </div>
     </div>
 
-    <?php include("footer.php"); ?>
+    <div class="row">
+        <section class="col-xs-12 col-md-4">
+            <h2>Über Uns</h2>
+            <figure>
+                <img src="img/beschreibung_pic.png" alt="Ueber Uns">
+                <figcaption class="text-limit">Wir sind das ganze Jahr für Sie und Ihr Objekt da und gehen gerne auf
+                    Ihre persönlichen Anliegen und Wünsche ein.
+                </figcaption>
+                <figcaption>
+                    <button class="btn btn-default pull-right zauberputz-button">Mehr</button>
+                </figcaption>
+            </figure>
+        </section>
+        <section class="col-xs-12 col-md-4">
+            <h2>Jobs</h2>
+            <figure>
+                <img src="img/jobs.jpg" alt="Jobs">
+                <figcaption class="text-limit">Interessiert an einem Job bei uns?</figcaption>
+                <figcaption>
+                    <button class="btn btn-default pull-right zauberputz-button">Mehr</button>
+                </figcaption>
+            </figure>
+        </section>
+    </div>
+</div>
+
+<?php include("footer.php"); ?>
 
 </body>
 
