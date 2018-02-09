d200b33ca7d67ed2058e75d4feff2100a30341e0
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-02-09 14:44:07 +0100

    Add "Hausbetreuung" page

--- hausbetreuung.php
+++ hausbetreuung.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Stefan
- * Date: 09/02/2018
- * Time: 14:37
- */
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <title>Hausbetreuung</title>
+    <?php include("head.php"); ?>
+</head>
+
+<body id="hausbetreuung">
+<div class="container">
+    <?php include("navbar.php"); ?>
+
+    <div class="trim"><img class="img img-responsive" src="img/header1.png" alt="Hausbetreuung Header"/></div>
+    <h1>Hausbetreuung</h1>
+    <p>Uns liegt viel an Ihrer Zufriedenheit und der Sauberkeit des Wohnobjektes. Deshalb ist unser Hausreinigungsangebot äußerst vielschichtig und variabel. Sie selbst bestimmen die Häufigkeit und Art der Reinigungsaktion. Unter anderem bieten wir folgende Dienste an:</p>
+    <ul>
+        <li>Kehren des Stiegenhauses</li>
+        <li>Nassreinigung des Stiegenhauses</li>
+        <li>Handläufe Nassreinigung</li>
+        <li>Lichtschalterreinigung</li>
+        <li>Reinigung der Eingangstüre</li>
+        <li>Kehren des Müllplatzes bzw. Müllraumes (bei Bedarf auch äußere Reinigung der Mistkübeldeckel)</li>
+        <li>Kehren des Gehsteiges</li>
+        <li>Entfernung der Spinnenweben</li>
+        <li>Fensterbrettreinigung</li>
+        <li>Reinigung der Aufzugkabine</li>
+        <li>Nassreinigung der Briefkästen</li>
+        <li>Entsorgung des Werbematerials
+        <li>Kehren des Fahrradraumes</li>
+        <li>Nassreinigung des Heizhauses</li>
+        <li>Reinigung des Kesselraums</li>
+        <li>Kellerreinigung</li>
+        <li>Grundreinigung (Lampen, Nebenräume, &hellip;)</li>
+        <li>Fenster putzen</li>
+        <li>Glühbirnenaustausch</li>
+        <li>Kehren des Hofes</li>
+        <li>Laub kehren</li>
+        <li>Gehsteig von Schnee säubern, kehren, streuen</li>
+        <li>Schnee von der Hauswand entfernen</li>
+        <li>Garagenreinigung</li>
+        <li>Dachbodenreinigung</li>
+    </ul>
+    <p>Falls Sie hier einen Dienst vermissen, keine Sorge! Dies soll Ihnen nur einen kleinen Überblick verschaffen, wir gehen natürlich gerne auf Ihre persönlichen Wünsche ein. Bei Fragen, kontaktieren Sie uns unter <a href="mailto:<?= $email ?>"><?= $email ?></a>.</p>
+</div>
+<?php include("footer.php"); ?>
+</body>
+</html>
