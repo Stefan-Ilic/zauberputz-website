301f02933260b4d36cbd1a00475c7eb9e4c1d945
Stefan Ilic <stefan.ilic@magic-it-services.at>
2022-08-07 18:05:22 +0200

    Prepare new website for deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-const path = require('path')
+const path = require('path');
+const withOptimizedImages = require('next-optimized-images');
 
-module.exports = {
+module.exports = withOptimizedImages({
+    handleImages: ['webp', 'svg', 'png', 'jpg'],
+
+    images: {
+      disableStaticImages: true
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
     trailingSlash: true,
     optimizeFonts: false,
     distDir: 'build',
-}
+    compress: true,
+    minify: true,
+})
