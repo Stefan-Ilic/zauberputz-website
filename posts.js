96668f8255db2cf9391e6597898f0a47091a3e6c
Stefan Ilic <stefan.ilic@magic-it-services.at>
2022-11-07 09:27:42 +0100

    Fix blog entry ordering

--- posts.js
+++ posts.js
@@ -7,7 +7,7 @@ export const getAllPosts = () => {
     const postsFullPaths = postFileNames.map(x => join(postsDirectory, x));
     const fileContents = postsFullPaths.map(x => fs.readFileSync(x, {encoding:'utf8'}));
     const firstLines = fileContents.map(x => x.split('\n')[0].substring(2));
-    const posts = firstLines.map((x, i) => ({...JSON.parse(x), link: `/blog/${postFileNames[i].slice(0, -3)}`}));
+    const posts = firstLines.map((x, i) => ({...JSON.parse(x), link: `/blog/${postFileNames[i].slice(0, -3)}`})).sort((a, b) => a.date < b.date ? 1 : -1);
 
     return posts;
 };
